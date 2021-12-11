4959e431dbd05e0f647d2a03a645c2db3df1236e
Budisetyo <budisetyo176@gmail.com>
2021-12-12 03:40:02 +0700

    make it better

--- src/database/dbMysql.js
+++ src/database/dbMysql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 
 // const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
