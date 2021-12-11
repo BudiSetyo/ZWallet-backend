3cf97eb225f25efbf1fed6303279852a25a0d624
Budisetyo <budisetyo176@gmail.com>
2021-12-12 03:42:41 +0700

    update

--- src/database/dbMysql.js
+++ src/database/dbMysql.js
@@ -8,8 +8,6 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-  multipleStatements: true,
 });
 
 db.connect((err) => {
