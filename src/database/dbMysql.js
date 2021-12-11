d0570c358edd44689d1c321404858daa3415a9fb
Budisetyo <budisetyo176@gmail.com>
2021-12-12 01:51:34 +0700

    new update

--- src/database/dbMysql.js
+++ src/database/dbMysql.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
 const mysql = require('mysql');
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 
 const db = mysql.createConnection({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
-  port: DB_PORT,
 });
 
 db.connect((err) => {
